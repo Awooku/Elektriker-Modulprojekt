559adac3838553534d8470425cb2977895edfec3
Awooku <qustieh@gmail.com>
2019-01-16 08:47:51 +0100

    Opdaterede datoer i kalenderjs.js
    titel

--- js/kalenderjs.js
+++ js/kalenderjs.js
@@ -31,14 +31,14 @@ $(document).ready(function() {
 		eventLimit: true, // allow "more" link when too many events
 		events: [
 			{
-				start: '2018-01-01',
-				end: '2018-01-06',
+				start: '2019-01-01',
+				end: '2019-01-06',
 				rendering: 'background'
 			},
 			{
 				title: 'Long event',
-				start: '2018-01-07',
-				end: '2018-01-10'
+				start: '2019-01-07',
+				end: '2019-01-10'
 			},
 			/*{
 				title: 'Background Yearview Dev',
@@ -49,56 +49,56 @@ $(document).ready(function() {
 			{
 				id: 999,
 				title: 'Repeating Event',
-				start: '2018-01-09T16:00:00'
+				start: '2019-01-09T16:00:00'
 			},
 			{
 				id: 999,
 				title: 'Repeating Event',
-				start: '2018-01-16T16:00:00'
+				start: '2019-01-16T16:00:00'
 			},
 			{
 				title: 'Long over months',
-				start: '2018-01-29',
-				end: '2018-02-02'
+				start: '2019-01-29',
+				end: '2019-02-02'
 			},
 			{
 				title: 'All Day Event',
-				start: '2018-03-01'
+				start: '2019-03-01'
 			},
 			{
 				title: 'Conference',
-				start: '2018-11-11',
-				end: '2018-11-13'
+				start: '2019-11-11',
+				end: '2019-11-13'
 			},
 			{
 				title: 'Meeting',
-				start: '2018-11-12T10:30:00',
-				end: '2018-11-12T12:30:00'
+				start: '2019-11-12T10:30:00',
+				end: '2019-11-12T12:30:00'
 			},
 			{
 				title: 'Lunch',
-				start: '2018-11-12T12:00:00'
+				start: '2019-11-12T12:00:00'
 			},
 			{
 				title: 'Meeting',
-				start: '2018-11-12T14:30:00'
+				start: '2019-11-12T14:30:00'
 			},
 			{
 				title: 'Happy Hour',
-				start: '2018-11-12T17:30:00'
+				start: '2019-11-12T17:30:00'
 			},
 			{
 				title: 'Dinner',
-				start: '2018-11-12T20:00:00'
+				start: '2019-11-12T20:00:00'
 			},
 			{
 				title: 'Birthday Party',
-				start: '2018-11-13T07:00:00'
+				start: '2019-11-13T07:00:00'
 			},
 			{
 				title: 'Click for Google',
 				url: 'http://google.com/',
-				start: '2018-03-28'
+				start: '2019-03-28'
 			}
 		]
 	});
