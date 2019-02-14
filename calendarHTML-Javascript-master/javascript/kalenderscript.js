5e4934e36b30d644dfedf056e4b9055e8711ec32
jordfolk <mj.elborough@hotmail.com>
2019-02-14 12:22:49 +0100

    1

--- calendarHTML-Javascript-master/javascript/kalenderscript.js
+++ calendarHTML-Javascript-master/javascript/kalenderscript.js
@@ -76,13 +76,10 @@ function showCalendar(month, year) {
         for (var j = 0; j < 7; j++) {
 
             if (i === 0 && j < firstDay) {
-                var ny = 0                
-                var cell = document.createElement("td");
-                var cellBox = document.createElement("div")                
+                var ny = 0
+                var cell = document.createElement("td");                
                 var cellText = document.createTextNode("");
                 cell.className = "tomdag";
-                cellBox.className = "dagbox";
-                row.appendChild(cellBox);
                 cell.appendChild(cellText);
                 row.appendChild(cell);
                 ny++;
@@ -95,11 +92,9 @@ function showCalendar(month, year) {
                 var dag3 = getElementByClassName("tomdag")[ny - 2];
                 var dag4 = getElementByClassName("tomdag")[ny - 3];
                 var dag5 = getElementByClassName("tomdag")[ny - 4];
-                dag1.parentNode.removeChild(dag1);
-                dag2.parentNode.removeChild(dag2);
-                dag3.parentNode.removeChild(dag3);
-                dag4.parentNode.removeChild(dag4);
-                dag5.parentNode.removeChild(dag5);
+                dag1.parentNode.removeCgh
+
+
                 var cell = document.createElement("td");
                 var cellText = document.createTextNode(date);
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
@@ -124,15 +119,12 @@ function showCalendar(month, year) {
             //opretter 
             else {
                 var cell = document.createElement("td");
-                var cellBox = document.createElement("div")    
                 var cellText = document.createTextNode(date);
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                     cell.classList.add("bg-info");
                 } // color today's date
-                cell.className = "dag";
-                cellBox.className = "dagbox";
-                row.appendChild(cellBox);
                 cell.appendChild(cellText);
+                cell.className = "dag";
                 row.appendChild(cell);
                 date++;
             }
