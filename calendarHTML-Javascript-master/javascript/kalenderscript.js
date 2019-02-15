8bfd517df8ee93536e6c6660f969208f39367be7
jordfolk <mj.elborough@hotmail.com>
2019-02-15 10:48:31 +0100

    det virker

--- calendarHTML-Javascript-master/javascript/kalenderscript.js
+++ calendarHTML-Javascript-master/javascript/kalenderscript.js
@@ -51,10 +51,15 @@ function showCalendar(month, year) {
         // creates a table row
         /*var row = document.createElement("tr");
         row.className = "liste";*/
-        if (i == 0){
+
+        if (i == 0) {
             var ugerow = document.createElement("tr");
             ugerow.className = "ugedagsliste";
+            document.getElementById("kalender-body").appendChild(ugerow);
+            console.log(ugerow);
         }
+
+
 
        for (var d = 0; d < 6; d++) {
 
@@ -69,8 +74,6 @@ function showCalendar(month, year) {
             }
 
             else if (i == 0 && d == 5) {
-                var tomrow = document.createElement("tr");
-                tomrow.id = "tomliste";
                 var row = document.createElement("tr");
                 row.className = "liste";
 
@@ -79,7 +82,6 @@ function showCalendar(month, year) {
             else {
                 var row = document.createElement("tr");
                 row.className = "linje";
-                console.log(d)
                 break;
             }
         }
@@ -144,76 +146,7 @@ function showCalendar(month, year) {
     }
 }
 
-/*
+
 function show3(month, year) {
-    
-    var firstDay = (new Date(year, month)).getDay()-1; //gør at første dag på ugen er en mandag i stedet for søndag
-    var daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    var tbl = document.getElementById("kalender-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-    
-    // creating all cells
-    var date = 1;
-
-    for (var m = 0; m < 3; m++) {
-
-        var kalenderbox = document.createElement("div");
-        kalenderbox.className = "kalenderbox";
-
-        for (var i = 0; i < 6; i++) {
-            // creates a table row
-            var row = document.createElement("tr");
-
-            //creating individual cells, filing them up with data.
-            for (var j = 0; j < 7; j++) {
-
-                var ugenavn = week[j]; 
-
-                if (j == 5 || j == 6) {
-                    date++;
-                }
-
-                else if (i === 0) {
-                    var cell = document.createElement("td");                
-                    var cellText = document.createTextNode(ugenavn);
-                    cell.className = "ugedag";
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
-                }
-
-                else if (i === 1 && j < firstDay) {
-                    var cell = document.createElement("td");                
-                    var cellText = document.createTextNode("");
-                    cell.className = "dag";
-                    cell.appendChild(cellText);
-                    row.appendChild(cell);
-                }
-
-                else if (date > daysInMonth) {
-                    break;
-                }
-
-                //opretter 
-                else {
-                    var cell = document.createElement("td");
-                    var cellText = document.createTextNode(date);
-                    if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                        cell.classList.add("bg-info");
-                    } // color today's date
-                    cell.appendChild(cellText);
-                    cell.className = "dag";
-                    row.appendChild(cell);
-                    date++;
-                }
-            }
-            tbl.appendChild(row); // appending each row into calendar body.
-        }
-    }
-}*/
+    showCalendar();
+}
