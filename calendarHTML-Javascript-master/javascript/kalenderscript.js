f23e28c19ee90b56af681761b8cb633ff75e2256
Awooku <qustieh@gmail.com>
2019-02-15 13:24:12 +0100

    kommentarer

--- calendarHTML-Javascript-master/javascript/kalenderscript.js
+++ calendarHTML-Javascript-master/javascript/kalenderscript.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var currentMonth = today.getMonth();
 var currentYear = today.getFullYear();
-var helekalender =document.getElementById("helekalender")
+var helekalender = document.getElementById("helekalender")
 var selectYear = document.getElementById("year");
 var selectMonth = document.getElementById("month");
 
@@ -27,82 +27,76 @@ function previous() {
 
 function showCalendar(month, year) {
     
-    var firstDay = (new Date(year, month)).getDay()-1; //gør at første dag på ugen er en mandag i stedet for søndag
+    var firstDay = (new Date(year, month)).getDay()-1;  //gør at første dag på ugen er en mandag i stedet for søndag
     var daysInMonth = 32 - new Date(year, month, 32).getDate();
 
     var tbl = document.getElementById("kalender-body"); // body of the calendar
 
-    var kalenderbox = document.createElement("div");
-    kalenderbox.className = "kalenderbox";
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
+    //var kalenderbox = document.createElement("div");
+    //kalenderbox.className = "kalenderbox"; 
+    
+    tbl.innerHTML = "";                                 // fjerner celler, bruges når man trykker på previous/next
+        
+    monthAndYear.innerHTML = months[month] + " " + year;// filing data about month and in the page via DOM.
     selectYear.value = year;
     selectMonth.value = month;
 
-    // creating all cells
+    // skaber alle celler
     var date = 1;
     for (var i = 0; i < 6; i++) {
-        // creates a table row
 
+        // skaber en reækken med ugedage (man, tir, ons, osv.)
         if (i == 0) {
             var ugerow = document.createElement("tr");
             ugerow.className = "ugedagsliste";
-            document.getElementById("kalender-body").appendChild(ugerow);
+            document.getElementById("kalender-body").appendChild(ugerow); // smider
             console.log(ugerow);
         }
 
        for (var d = 0; d < 6; d++) {
 
-            var ugenavn = week[d]; 
+            var ugenavn = week[d]; //var ugenavn = arrayet hvor navnene på ugedagene står
 
             if (i == 0 && d != 5) {
                 var cell = document.createElement("td");                
                 var cellText = document.createTextNode(ugenavn);
                 cell.className = "ugedag";
                 cell.appendChild(cellText);
-                ugerow.appendChild(cell);
+                ugerow.appendChild(cell); // forklar marcus pls
             }
 
             else if (i == 0 && d == 5) {
                 var row = document.createElement("tr");
-                row.className = "liste";
-
+                row.className = "liste"; //første række efter ugenavnene får klassen liste
             }
 
             else {
                 var row = document.createElement("tr");
-                row.className = "linje";
+                row.className = "linje"; //resten af rækkerne kaldes for linje
                 break;
             }
         }
         var ny = 0;  
 
-        //creating individual cells, filing them up with data.
+        //skaber de individuelle celler og fylder dem med data
         for (var j = 0; j < 7; j++) {
 
             if (i === 0 && j < firstDay) {
                 var cell = document.createElement("td");                
                 var cellText = document.createTextNode("");
-                cell.className = "tomdag";
+                cell.className = "tomdag"; //vi laver en td som kaldes for tomdag for at gøre det nemmere at fjerne dem i css
                 cell.appendChild(cellText);
                 row.appendChild(cell);
                 ny++;
             }
 
             else if (ny == 5) {
-
-                var tommedage = document.getElementsByClassName("tomdag");
-                console.log(tommedage);
-
                 var cell = document.createElement("td");
                 var cellText = document.createTextNode(date);
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                     cell.classList.add("bg-info");
                 } // color today's date
-                row.id = "tomx5";
+                row.id = "tomx5"; //hvis 'var ny' bliver talt op på til 5 kalder vi den række for tomx5 så det bliver nemmere at fjerne i css
 
                 date++;
                 ny++;
@@ -132,10 +126,9 @@ function showCalendar(month, year) {
                 date++;
             }
         }
-        tbl.appendChild(row); // appending each row into calendar body.
+        tbl.appendChild(row); // smider vær reække ind i kalenderen
     }
 }
-
 
 function show3(month, year) {
     showCalendar();
