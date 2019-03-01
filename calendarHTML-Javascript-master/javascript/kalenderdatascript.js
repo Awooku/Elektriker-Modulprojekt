fd1a73b8b395787fe4ab818fb8fb136d638ae81b
jordfolk <mj.elborough@hotmail.com>
2019-03-01 14:27:41 +0100

    gg

---
+++ calendarHTML-Javascript-master/javascript/kalenderdatascript.js
@@ -0,0 +1,11 @@
+var text = '{"startdato":"2019-03-14", "slutdato":"2019-04-30", "skole":"TEC", "modultal":"1.5"}';
+var obj = JSON.parse(text);
+var startdato = new Date(obj.startdato);
+var slutdato = new Date(obj.slutdato);
+
+document.getElementById("demo").innerHTML = startdato + ", " + slutdato;
+
+
+var antaldage = slutdato - startdato;
+var antaldage = (antaldage / (60*60*24*1000));
+console.log(antaldage);
