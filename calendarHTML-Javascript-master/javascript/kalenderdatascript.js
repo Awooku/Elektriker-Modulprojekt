e6b82e97d21956618242568f429e55ec32ea09cb
jordfolk <mj.elborough@hotmail.com>
2019-03-28 08:03:39 +0100

    Update kalenderdatascript.js

--- calendarHTML-Javascript-master/javascript/kalenderdatascript.js
+++ calendarHTML-Javascript-master/javascript/kalenderdatascript.js
@@ -356,6 +356,7 @@ function events() {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     var modulnr = 0;
     var antalmodul = 0;
 
@@ -366,6 +367,10 @@ function events() {
 =======
     var text = '{"startdato":"2019-4-20", "slutdato":"2019-5-30", "skole":"TEC", "modultal":"1.5"}';
     var obj = JSON.parse(text);
+=======
+    var text = '{"startdato":"2019-4-20", "slutdato":"2019-5-30", "skole":"TEC", "modultal":"1.5"}';
+    var obj = JSON.parse(text);
+>>>>>>> parent of 3a1d4e2... fejlsøgning
 =======
     var text = '{"startdato":"2019-4-20", "slutdato":"2019-5-30", "skole":"TEC", "modultal":"1.5"}';
     var obj = JSON.parse(text);
