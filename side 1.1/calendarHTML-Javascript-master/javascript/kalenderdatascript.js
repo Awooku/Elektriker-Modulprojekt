de812c7237603a1cda93ad03f768fc22a4919d84
Awooku <31002082+Awooku@users.noreply.github.com>
2019-06-21 08:09:22 +0200

    opdateringer
    opdaterede 900p css
    ændret jquery script til at vise en dagsoversigt når du trykker på en dag
    tilføjet kommentarer til kalenderdatascript.js
    opdaterede funktioner i thescript.js til at være kortere og mere funktionelle
    opdaterede trejdegang.html så den kan bruge de nye funktioner i thescript.js

--- side 1.1/calendarHTML-Javascript-master/javascript/kalenderdatascript.js
+++ side 1.1/calendarHTML-Javascript-master/javascript/kalenderdatascript.js
@@ -431,23 +431,23 @@ function showmonth() {
 function jsonHandler() {
     var jText = '[{"id":5,"pladser":30,"startdato":"31/12/2019","slutdato":"31/01/2020","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 1","moduldata_id":"1.1","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":6,"pladser":30,"startdato":"01/01/2019","slutdato":"31/01/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 1","moduldata_id":"1.1","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":7,"pladser":30,"startdato":"01/02/2019","slutdato":"28/02/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 2","moduldata_id":"1.2","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":8,"pladser":30,"startdato":"01/03/2019","slutdato":"29/03/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 2","moduldata_id":"1.3","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":9,"pladser":30,"startdato":"01/04/2019","slutdato":"30/04/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 3","moduldata_id":"2.1","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":10,"pladser":30,"startdato":"01/05/2019","slutdato":"31/05/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 3","moduldata_id":"2.2","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},{"id":11,"pladser":30,"startdato":"03/06/2019","slutdato":"31/06/2019","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 4","moduldata_id":"3.1","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"}]';
 
-    var jObj = JSON.parse(jText);
+    var jObj = JSON.parse(jText); //gør jText filen om til et JSON object
 
-    var skoler = ["Skole 1", "Skole 2", "Skole 3", "Skole 4"];
+    var skoler = ["Skole 1", "Skole 2", "Skole 3", "Skole 4"]; //tester
 
     for (j = 0; j < jObj.length; j++) {
 
-        jObjA[j] = jObj[j];
+        jObjA[j] = jObj[j]; //gemmer det enkelte json object i et array hver gang for loopet kører
 
-        var newSTDate = jObj[j].startdato.split("/").reverse().join("-");
-        startdato[j] = new Date(newSTDate);
+        var newSTDate = jObj[j].startdato.split("/").reverse().join("-"); //eksempel: 31/12/2019 bilver splittet så den ligner 31 12 2019, bliver reverset til 2019 12 31 og bliver til 2019-12-31 på join("-")
+        startdato[j] = new Date(newSTDate); //gør at den dato vi har fået fra newSTDate bliver en reel dato, og smider den ind i et array
     
-        var newSLDate = jObj[j].slutdato.split("/").reverse().join("-");
+        var newSLDate = jObj[j].slutdato.split("/").reverse().join("-"); //gør det samme som ovenover
         slutdato[j] = new Date(newSLDate);
 
-        skoleID[j] = jObj[j].skole_id;
-        modultal[j] = jObj[j].moduldata_id;
-        eventID[j] = jObjA[j].id;
+        skoleID[j] = jObj[j].skole_id; //skole_id bliver smidt ind i et array hver gang loopet kører
+        modultal[j] = jObj[j].moduldata_id; //moduldata_id bliver smidt ind i et array hver gang loopet kører
+        eventID[j] = jObjA[j].id; //id bliver smidt ind i et array hver gang loopet kører
 
         //console.log(j);
         //console.log(startdato);
@@ -459,13 +459,14 @@ function jsonHandler() {
     console.log(eventID);
     console.log(skoler);
     
+    //nedenstående er tests som forbereder mig på fremtiden
 
     if (skoleID[0] == skoler[0]) {
-        console.log("success");
+        console.log(skoleID[0]);
     }
 
     if (skoleID[2] !== skoler[2]) {
-        console.log("failure");
+        console.log(skoleID[2]);
     }    
 
     for (var i = 0; i < eventID.length; i++) {
@@ -473,6 +474,7 @@ function jsonHandler() {
             console.log(jObjA[i]);
         }
     }
+
 
     //console.log(lokalID[1])
 }
