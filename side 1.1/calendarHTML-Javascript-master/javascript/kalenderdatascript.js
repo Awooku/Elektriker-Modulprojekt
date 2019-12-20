669442d563b8e473d92234871360b60e6eb218bc
Awooku <qustieh@gmail.com>
2019-12-20 10:41:00 +0100

    sidste småændringer før ferie

--- side 1.1/calendarHTML-Javascript-master/javascript/kalenderdatascript.js
+++ side 1.1/calendarHTML-Javascript-master/javascript/kalenderdatascript.js
@@ -393,9 +393,9 @@ function show3() {
         currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1; //beregner nuværende måned
     }
     k = 0; //bruges til at insætte måneder i div tags
-    jsonHandler(); //henter datoer der skal bruges
-    events(); //henter indhold til kalenderen
-    swapSheet('/css/kalenderstyleKvartal.css'); //skifter css om til 3 måneders visning
+    jsonParser(); //henter datoer der skal bruges
+    events(jObjA); //henter indhold til kalenderen
+    swapSheet('calendarHTML-Javascript-master/css/kalenderstyleKvartal.css'); //skifter css om til 3 måneders visning
 }
 
 //Viser hele år
@@ -424,8 +424,8 @@ function showyear() {
         currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1; //beregner nuværende måned
     }
     k = 0; //bruges til at insætte måneder i div tags
-    jsonHandler(); //henter datoer der skal bruges
-    events(); //henter indhold til kalenderen
+    jsonParser(); //henter datoer der skal bruges
+    events(jObjA); //henter indhold til kalenderen
     swapSheet('calendarHTML-Javascript-master/css/kalenderstyle.css'); //skifter css om til årlig visning
 }
 
@@ -445,9 +445,9 @@ function showmonth() {
     showCalendar(currentMonth, currentYear); //henter kalender programmet
     m = 1;  // bruges i next og previous 
     k = 0;  // bruges til at insætte måneder i div tags
-    jsonHandler(); //henter datoer der skal bruges
-    events(); //henter indhold til kalenderen
-    swapSheet('/css/kalenderstyleMåned.css'); //skifter css om til 1 månededs visning
+    jsonParser(); //henter datoer der skal bruges
+    events(jObjA); //henter indhold til kalenderen
+    swapSheet('calendarHTML-Javascript-master/css/kalenderstyleMåned.css'); //skifter css om til 1 månededs visning
 }
 
 //--------------------------------------------------------------------------------JSON--------------------------------------------------------------------------------->
@@ -467,73 +467,65 @@ function jsonParser() {
     '{"id":14,"pladser":14,"startdato":"01/11/2019","slutdato":"14/01/2020","reserverede_pladser":12,"synlig":"ja","skole_id":"Skole 7","moduldata_id":"2.3","region_id":"Midtjylland","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},' +
     '{"id":15,"pladser":22,"startdato":"29/11/2019","slutdato":"10/01/2020","reserverede_pladser":19,"synlig":"ja","skole_id":"Skole 1","moduldata_id":"1.1","region_id":"Midtjylland","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},' +
     '{"id":16,"pladser":19,"startdato":"02/12/2019","slutdato":"21/01/2021","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 7","moduldata_id":"2.3","region_id":"Nordjylland","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"},' +
-    '{"id":17,"pladser":30,"startdato":"04/12/2019","slutdato":"19/05/2021","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 2","moduldata_id":"2.3","region_id":"Nordjylland","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"}]';  //var jText = '{!! $events !!}';
-  
-              /* 
-              '{"id":14,"pladser":30,"startdato":"03/06/2020","slutdato":"30/06/2020","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 3","moduldata_id":"4.1","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"}]';*/
+    '{"id":17,"pladser":30,"startdato":"04/12/2019","slutdato":"19/05/2021","reserverede_pladser":15,"synlig":"ja","skole_id":"Skole 2","moduldata_id":"2.3","region_id":"Nordjylland","created_at":"2019-06-13 06:17:35","updated_at":"2019-06-13 06:17:35"}]';  
 
-  // var jObj = JSON.parse(jText); //gør jText filen om til et JSON object
-  var jObj = JSON.parse(jText);
+    //var jText = '{!! $events !!}';
 
-  for (i = 0; i < jObj.length; i++) {
-      jObjA[i] = jObj[i]; //gemmer det enkelte json object i et array hver gang for loopet kører
-  }
+    // var jObj = JSON.parse(jText); //gør jText filen om til et JSON object
+    var jObj = JSON.parse(jText);   
+    for (i = 0; i < jObj.length; i++) {
+        jObjA[i] = jObj[i]; //gemmer det enkelte json object i et array hver gang for loopet kører
+    }
+    jsonHandler(jObjA);
 
-  jsonHandler(jObjA);
-
-  //Loopet kører for hvert modul der bliver sendt igennem
+    //Loopet kører for hvert modul der bliver sendt igennem
 
 }
 
 function jsonHandler(jsonObjectArray) {
+    for (i = 0; i < jsonObjectArray.length; i++) {
 
-  for (i = 0; i < jsonObjectArray.length; i++) {
-
-      var newSTDate = dateSplitter(jsonObjectArray[i].startdato);
-      var newEDate = dateSplitter(jsonObjectArray[i].slutdato);
-
-      slutdato[i] = newEDate;
-      startdato[i] = newSTDate; //gør at den dato vi har fået fra newSTDate bliver en reel dato, og smider den ind i et array
-      if (startdato[i] >= (currentYear + 1) + "-1-" + "1") { //hvis start dato er større eller lig med nuværende år + 1 - 1 + 1                                                      <---- hvad fuck
-          break;
-      }
-        else {   
-            synlig[i] = jsonObjectArray[i].synlig; //synlig bliver smidt ind i et array hver gang loopet kører
-            skoleID[i] = jsonObjectArray[i].skole_id; //skole_id bliver smidt ind i et array hver gang loopet kører
-            modultal[i] = jsonObjectArray[i].moduldata_id; //moduldata_id bliver smidt ind i et array hver gang loopet kører
-            eventID[i] = jsonObjectArray[i].id; //id bliver smidt ind i et array hver gang loopet kører
+        var newSTDate = dateSplitter(jsonObjectArray[i].startdato);
+        var newEDate = dateSplitter(jsonObjectArray[i].slutdato);   
+        slutdato[i] = newEDate;
+        startdato[i] = newSTDate; //gør at den dato vi har fået fra newSTDate bliver en reel dato, og smider den ind i et array
+        if (startdato[i] >= (currentYear + 1) + "-1-" + "1") { //hvis start dato er større eller lig med nuværende år + 1 - 1 + 1                                                      <---- hvad fuck
+            break;
+        }
+          else {   
+              synlig[i] = jsonObjectArray[i].synlig; //synlig bliver smidt ind i et array hver gang loopet kører
+              skoleID[i] = jsonObjectArray[i].skole_id; //skole_id bliver smidt ind i et array hver gang loopet kører
+              modultal[i] = jsonObjectArray[i].moduldata_id; //moduldata_id bliver smidt ind i et array hver gang loopet kører
+              eventID[i] = jsonObjectArray[i].id; //id bliver smidt ind i et array hver gang loopet kører
         }
     }
 }
 
 function dateSplitter(dateToBeSplit) {
-  var newSplitDate = dateToBeSplit.split("/").reverse().join("-"); //splitter dato på /, omvender tallene, smider sammen på - (xx/xx/xxxx -> xxxx-xx-xx)
-  newSplitDate = newSplitDate.split("-0").join("-"); //fjerner 0'er ellers klager siden (xxxx-0x-0x -> xxxx-x-x)
-  return newSplitDate;
+    var newSplitDate = dateToBeSplit.split("/").reverse().join("-"); //splitter dato på /, omvender tallene, smider sammen på - (xx/xx/xxxx -> xxxx-xx-xx)
+    newSplitDate = newSplitDate.split("-0").join("-"); //fjerner 0'er ellers klager siden (xxxx-0x-0x -> xxxx-x-x)
+    return newSplitDate;
 }
 
-function getDateArray(start, end) {        
-  
-  var fDatesArray = new Array(); //nyt array
-  var dt = new Date(start); //start (startdato[i]) bliver lavet til en dato
-  var end = new Date(end); //end (slutdato[i]) bliver lavet til en dato
-
-  while(dt.toLocaleDateString() != end.toLocaleDateString()){ //mens startdatoen er mindre end eller lig med slutdatoen        
-      fDatesArray.push(new Date(dt)); //laver en dato ud af dt og smider den på arrayet
-      dt.setDate(dt.getDate() + 1);
-      if (dt.toLocaleDateString() == end.toLocaleDateString()) { // der er chance for at dt == end når while loopet er færdigt, er dette tilfældet skal vi indsætte datoen efter dt
-          fDatesArray.push(new Date(dt)); 
-      }
-  }
-  return fDatesArray;
+function getDateArray(start, end) {          
+    var fDatesArray = new Array(); //nyt array
+    var dt = new Date(start); //start (startdato[i]) bliver lavet til en dato
+    var end = new Date(end); //end (slutdato[i]) bliver lavet til en dato   
+    while(dt.toLocaleDateString() != end.toLocaleDateString()){ //mens startdatoen er mindre end eller lig med slutdatoen        
+        fDatesArray.push(new Date(dt)); //laver en dato ud af dt og smider den på arrayet
+        dt.setDate(dt.getDate() + 1);
+        if (dt.toLocaleDateString() == end.toLocaleDateString()) { // der er chance for at dt == end når while loopet er færdigt, er dette tilfældet skal vi indsætte datoen efter dt
+            fDatesArray.push(new Date(dt)); 
+        }
+    }
+    return fDatesArray;
 }  
 
 function dateFixer(arrayOfDates) {
-  arrayOfDates.setHours(0, -arrayOfDates.getTimezoneOffset(), 0, 0); //gør noget (stackoverflow siger "removing the timezone offset and 12 hours")
-  arrayOfDates = arrayOfDates.toISOString().split("T")[0]; //datoer bliver til en ISOString (yyyy-mm-ddThh:mm:ss), men bliver splittet på T så det bliver til et array, tager den første del arrayet
-  arrayOfDates = arrayOfDates.split("-0").join("-"); //splitter på "-0", sætter sammen igen på "-" (xxxx-0x-0x -> xxxx-x-x)
-
-  return arrayOfDates;
+    arrayOfDates.setHours(0, -arrayOfDates.getTimezoneOffset(), 0, 0); //gør noget (stackoverflow siger "removing the timezone offset and 12 hours")
+    arrayOfDates = arrayOfDates.toISOString().split("T")[0]; //datoer bliver til en ISOString (yyyy-mm-ddThh:mm:ss), men bliver splittet på T så det bliver til et array, tager den første del arrayet
+    arrayOfDates = arrayOfDates.split("-0").join("-"); //splitter på "-0", sætter sammen igen på "-" (xxxx-0x-0x -> xxxx-x-x)   
+    return arrayOfDates;
 }
 
 //---------------------------------------------------------------------------Events Handler---------------------------------------------------------------------------->
@@ -541,7 +533,6 @@ function dateFixer(arrayOfDates) {
 function events(allEvents) { //placerer events
     var hasEventList = document.getElementsByClassName("hasEvent"); //fanger elementer som har klassen hasEvent    
     jsonHandler(allEvents);
-    //debugger;
 
     //TODO, når allevents er mindre end jObjA skal alle events på siden cleares
     //efter nærmere eftertanke ved jeg ikke hel vad dette skal gøre
